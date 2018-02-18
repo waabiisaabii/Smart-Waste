366befd5f6c8e9f73da95028e9a2e9dd4ae02a59
Yachen <yachenlin1203@gmail.com>
2018-02-18 11:43:32 -0500

    Add model for storing bin status and add a function that returns a JSON format data containing all binstatus

--- server/smartWaste/bins/models.py
+++ server/smartWaste/bins/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Bin(models.Model):
+	binID = models.IntegerField()
+	geoLocation = models.TextField(max_length=420)
+	binStatus = models.IntegerField()
+	def __unicode__(self):
+		return "{0}, {1}, {2}".format(self.binID, self.geoLocation, self.binStatus)
+
--- server/smartWaste/bins/views.py
+++ server/smartWaste/bins/views.py
@@ -5,15 +5,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse, Http404
+from bins.models import *
+# from bins.forms import *
+from django.core import serializers
 
 def getSensorInfo(requests):
 	if (requests.method != 'GET'):
 		raise Http404
-	
+	geoLocation = requests.get('geo')
+	binStatus = requests.get('status')
+	pk = requests.get('pk')
 
-
+	newBin = Bin.objects.create(binID=pk,
+		geoLocation=geoLocation,
+		binStatus=binStatus)
+	newBin.save()
 	return HttpResponse('well', content_type='text/plain')
 
 def sendBackToPhone(requests):
-	print 'hello'
-	return HttpResponse('well', content_type='text/plain')
+	allBins = Bin.objects.all()
+	response_text = serializers.serialize("json", allBins)
+	return HttpResponse(response_text, content_type="application/json")
