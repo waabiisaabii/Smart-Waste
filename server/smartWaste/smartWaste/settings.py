31e697bc4427ec6b08779ca9da2cf1f174684965
Yachen <yachenlin1203@gmail.com>
2018-04-02 14:15:58 -0400

    Finish Django deployment checklist

--- server/smartWaste/smartWaste/settings.py
+++ server/smartWaste/smartWaste/settings.py
@@ -20,15 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wloxb+$g^$=0zx%707f0me=b#7kd8-=l)#wnm+k-*t#e&ue&we'
+# SECRET_KEY = 'wloxb+$g^$=0zx%707f0me=b#7kd8-=l)#wnm+k-*t#e&ue&we'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 # Set the project path as a variable to be used below
-PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__)) + '/'
+# PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__)) + '/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Application definition
@@ -84,6 +87,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# import dj_database_url
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
@@ -126,3 +132,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = PROJECT_ROOT + 'static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SECURE_HSTS_SECONDS = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
