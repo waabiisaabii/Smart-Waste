02417f0cb9098d4fb144254eb1e8532473eee4b1
Yachen <yachenlin1203@gmail.com>
2018-04-23 17:33:49 -0400

    Reconstruct the structure of smart waste repo

---
+++ GoogleMaps/app/src/main/java/com/smartwaste/googlemaps/BinStatus.java
@@ -0,0 +1,199 @@
+package com.smartwaste.googlemaps;
+
+import android.os.AsyncTask;
+import android.os.Parcelable;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * Getting trash Bin status from Smart Waste server.
+ */
+public class BinStatus extends AsyncTask<Void, Void, List<BinStatus.Item>> {
+    private static final String BIN_ID_NAME = "binID";
+    private static final String GEO_LOCATION_NAME = "geoLocation";
+    private static final String BIN_STATUS_NAME = "binStatus";
+    private static final String REQUEST_URGENT = "requestUrgent";
+    private static final String LAST_PICKUP_TIME = "lastPickUpTime";
+
+    private URL serverUrl;
+
+    protected BinStatus(String serverUrlStr) throws MalformedURLException {
+        serverUrl = new URL(serverUrlStr);
+    }
+
+    private List<Item> getBinStatusFromServer() {
+        List<Item> result = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(serverUrl.openStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                JSONParser jsonParser = new JSONParser();
+                JSONArray jsonArray = (JSONArray) jsonParser.parse(inputLine);
+                result.addAll(processJSONArray(jsonArray));
+            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    private List<Item> processJSONArray(JSONArray jsonArray) {
+        List<Item> result = new ArrayList<>();
+        for (Object obj : jsonArray) {
+            JSONObject jsonObject = (JSONObject) obj;
+            Map<String, Object> itemMap = (Map<String, Object>) jsonObject.get("fields");
+            try {
+                long binId = (long) itemMap.get(BIN_ID_NAME);
+                String geoLocationStr = (String) itemMap.get(GEO_LOCATION_NAME);
+                long binStatus = (long) itemMap.get(BIN_STATUS_NAME);
+                long requestUrgent = (long) itemMap.get(REQUEST_URGENT);
+                String lastPickUpTime = (String) itemMap.get(LAST_PICKUP_TIME);
+
+                Item item = new Item(binId, binStatus, geoLocationStr, requestUrgent, lastPickUpTime);
+                result.add(item);
+
+            } catch (Exception e) {
+                // ignore illegal case
+                continue;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    protected List<Item> doInBackground(Void... voids) {
+        try {
+            return getBinStatusFromServer();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    public static class Item implements Serializable {
+        private long binId;
+        private long binStatus;
+        private long requestUrgent;
+        private String lastPickUpTime;
+        private double lat, lon;
+
+        public String getLatLonStr() {
+            return latLonStr;
+        }
+
+        private String latLonStr;
+
+        public Item(long binId, long binStatus, String geoLocationStr,
+                    long requestUrgent, String lastPickUpTime) {
+            this.binId = binId;
+            this.binStatus = binStatus;
+            double[] coords = parseGeoLocation(geoLocationStr);
+            if (coords == null) {
+                lat = 0.0;
+                lon = 0.0;
+            } else {
+                lat = coords[0];
+                lon = coords[1];
+            }
+            this.requestUrgent = requestUrgent;
+            this.lastPickUpTime = lastPickUpTime;
+            latLonStr = lat + ", " + lon;
+        }
+
+        private static double[] parseGeoLocation(String geoLocationInStr) {
+            if (geoLocationInStr == null) {
+                return null;
+            }
+            String[] coords = geoLocationInStr.split(",");
+            double lat = Double.parseDouble(coords[0]);
+            double lon = Double.parseDouble(coords[1]);
+            return new double[]{lat, lon};
+        }
+
+        /**
+         * Getter for requestUrgent.
+         *
+         * @return requestUrgent
+         */
+        public long getRequestUrgent() {
+            return requestUrgent;
+        }
+
+        /**
+         * Getter for lastPickUpTime.
+         *
+         * @return lastPickUpTime
+         */
+        public String getLastPickUpTime() {
+            return lastPickUpTime;
+        }
+
+        /**
+         * Getter for binId.
+         *
+         * @return binId
+         */
+        public long getBinId() {
+            return binId;
+        }
+
+        /**
+         * Getter for binStatus.
+         *
+         * @return binStatus
+         */
+        public long getBinStatus() {
+            return binStatus;
+        }
+
+        /**
+         * Getter for latitude.
+         *
+         * @return latitude
+         */
+        public double getLat() {
+            return lat;
+        }
+
+        /**
+         * Getter for longitude.
+         *
+         * @return longitude
+         */
+        public double getLon() {
+            return lon;
+        }
+
+        public String getEncodedString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append(binId).append("#");
+            sb.append(binStatus == 0 ? "Empty" : "Full").append("#");
+            sb.append(lat).append(", ").append(lon);
+            return sb.toString();
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Bin ID: ").append(binId);
+            sb.append(String.format("\n"));
+            sb.append(": (").append(lat).append(", ").append(lon).append(")");
+            return sb.toString();
+        }
+    }
+}
---
+++ GoogleMaps/app/src/main/java/com/smartwaste/googlemaps/MapsActivity.java
@@ -0,0 +1,206 @@
+package com.smartwaste.googlemaps;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.data.kml.KmlLayer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class MapsActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnMapClickListener {
+
+
+    public static final String serverURL = "https://peaceful-island-64716.herokuapp.com/iot/";
+    public static final String serverActionGetBinStatus = serverURL + "returnJSON";
+    public static final String serverActionDamageReport = serverURL + "reportDamage";
+    public static final String serverActionGetAllDamageReport = serverURL + "getAllDamageReports";
+    public static final String LAT_LON = "LatLon";
+    private GoogleMap mMap;
+    private Button damageReportButton;
+    private Marker selectedMarker;
+    private Map<String, BinStatus.Item> itemMap;
+
+    /**
+     * Getter for map.
+     *
+     * @return map
+     */
+    public GoogleMap getmMap() {
+        return mMap;
+    }
+
+    private void createKMLLayer() {
+        try {
+            KmlLayer kmlLayer = new KmlLayer(getmMap(), R.raw.kml, getApplicationContext());
+            kmlLayer.addLayerToMap();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add kml layer to the map
+        createKMLLayer();
+
+        // Display trash bins
+        itemMap = new HashMap<>();
+        displayTrashBins(serverActionGetBinStatus);
+
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMapClickListener(this);
+
+        damageReportButton = findViewById(R.id.damageReportButton);
+
+    }
+
+    private void displayTrashBins(String url) {
+        try {
+
+            List<BinStatus.Item> items = new BinStatus(url).execute().get();
+            for (BinStatus.Item item : items) {
+                itemMap.put(item.toString(), item);
+                LatLng coord = new LatLng(item.getLat(), item.getLon());
+                System.out.println(coord);
+                int binColor = item.getBinStatus() == 0 ? R.drawable.greenl : R.drawable.redl;
+                Marker newMarker = mMap.addMarker(new MarkerOptions()
+                        .position(coord)
+                        .icon(BitmapDescriptorFactory.fromResource(binColor)));
+                newMarker.setTag(item.toString());
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(coord));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        selectedMarker = marker;
+        Toast.makeText(this, (CharSequence) marker.getTag(), Toast.LENGTH_SHORT).show();
+
+        System.out.println("> ######marker clicked" + marker.getTag());
+
+        damageReportButton.setVisibility(View.VISIBLE);
+        return false;
+    }
+
+    public void damageReport(View view) {
+        // Do something in response to button click
+
+        System.out.print(">####### damage report button clicked! ");
+        System.out.println(selectedMarker.getTag());
+
+        Intent intent = new Intent(this, ReportDamageActivity.class);
+        String itemKey = selectedMarker.getTag().toString();
+
+        intent.putExtra(LAT_LON, itemMap.get(itemKey));
+        startActivityForResult(intent, RESULT_FIRST_USER);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        System.out.println(">>>>>>map clicked");
+        damageReportButton.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            String popUpMsg;
+            if (resultCode == RESULT_OK) {
+                System.out.println("####result ok!!!!");
+                popUpMsg = "Report submitted.";
+
+            } else {
+                System.out.println("####result bad");
+                popUpMsg = "Failed to submit report.";
+            }
+
+            Snackbar.make(findViewById(R.id.slidingLayout), popUpMsg,
+                    Snackbar.LENGTH_LONG)
+                    .show();
+        }
+    }
+}
